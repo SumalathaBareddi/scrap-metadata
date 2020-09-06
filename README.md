d0fe986daeacac8bca81051a119f368d92769e82
Sumalatha <sumalathabareddi@gmail.com>
2020-09-06 10:31:17 +0530

    added tested successfully

---
+++ README.md
@@ -0,0 +1,42 @@
+[![Node.Js  Logo](http://sub1.kevinchisholm.com/blog/images/node-js-logo.png)](https://nodejs.org/)
+
+
+## Provides metadata and image info for any web page as JSON 
+
+## Dependencies
+
+What you need to run this app:
+
+* [Node.Js](https://nodejs.org)
+
+## Instructions
+
+### Setup
+
+* Clone this repo:
+
+```
+git clone https://github.com/SumalathaBareddi/scrap-metadata.git
+```
+
+* Run this command:
+
+```
+npm install
+```
+
+### Running the code
+
+### 1) Execute this command in your terminal:
+
+```
+node app.js
+```
+
+#### 2) Testing:
+
+```
+a. POSTMAN
+
+make a post request with to http://localhost:2020/get-meta 
+```
---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict;';
+
+const express = require('express'),
+          app = express(),
+   bodyParser = require('body-parser'),
+       logger = require('morgan');
+         cors = require('cors');
+         Meta = require('html-metadata-parser');
+
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
+app.post('/get-meta',async(req,res)=>{
+    try {
+        //bodyParams
+    const bodyParams = req.body;
+    //body parameters extraction
+    const {url} = bodyParams;
+        //parameter checking 
+    if(url){
+        //extracting the result from package 
+        var result = await Meta.parser(req.body.url);    
+        //sending success response to user    
+        res.status(200).send(result);
+    }else{
+        res.status(404).send({status:'faiure',message: 'url canot be null!'});
+    }
+    
+        
+    } catch (error) {
+        //sending error response to user    
+        res.status(500).send(error);
+    }
+
+
+});
+
+const port = 2020;
+//listen for an HTTP request
+app.listen(port,console.log(`The server is listening on port ${port}`));
